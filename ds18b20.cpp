0fc4bec914a8c4d3c367b5aa207da53ea0e59114
Mo-SW-Dev <40509245+Mo-SW-Dev@users.noreply.github.com>
2020-03-01 17:29:17 +0100

    Add files via upload

---
+++ ds18b20.cpp
@@ -0,0 +1,106 @@
+#include "pxt.h"
+#include <cstdint>
+#include <math.h>
+
+using namespace pxt;
+
+namespace DS18B20 {
+    
+    MicroBitPin  pin = &uBit.io.P0;
+    MicroBit uBit;
+
+    int16_t Temperature(int p) {
+        switch(p){
+          case 0: pin = &uBit.io.P12; break; //P0
+          case 1: pin = &uBit.io.P0; break; //P1
+          case 2: pin = &uBit.io.P1; break; //P2
+          case 3: pin = &uBit.io.P16; break; //P3          
+          case 4: pin = &uBit.io.P2; break; //C16
+          case 5: pin = &uBit.io.P8; break; //C17
+          case 6: pin = &uBit.io.P18; break; //C18
+          case 7: pin = &uBit.io.P19; break; //C19
+          default: pin = &uBit.io.P0;
+        }
+
+        init();
+        writeByte(0xCC);
+        convert();
+        init();
+        writeByte(0xCC);
+        writeByte(0xBE);
+        int b1 = readByte();
+        int b2 = readByte();
+        int16_t temp = (b2 << 8 | b1);
+        return temp * 100 / 16;
+    }
+
+    uint8_t init() {
+        pin.setDigitalValue(0);
+        for (volatile uint16_t i = 0; i < 600; i++);
+        pin.setDigitalValue(1);
+        for (volatile uint8_t i = 0; i < 30; i++);
+        int b = pin.getDigitalValue();
+        for (volatile uint16_t i = 0; i < 600; i++);
+        return b;
+    }
+
+
+    void writeBit(int b) {
+        int delay1, delay2;
+        if (b == 1) {
+            delay1 = 1;
+            delay2 = 80;
+        } else {
+            delay1 = 75;
+            delay2 = 6;
+        }
+        pin.setDigitalValue(0);
+        for (uint8_t i = 1; i < delay1; i++);
+        pin.setDigitalValue(1);
+        for (uint8_t i = 1; i < delay2; i++);
+    }
+
+    void writeByte(int byte) {
+        int i;
+        for (i = 0; i < 8; i++) {
+            if (byte & 1) {
+                writeBit(1);
+            } else {
+                writeBit(0);
+            }
+            byte = byte >> 1;
+        }
+    }
+
+    int readBit() {
+        volatile int i;
+        pin.setDigitalValue(0);
+        pin.setDigitalValue(1);
+        //for (i = 1; i < 20; i++);
+        int b = pin.getDigitalValue();
+        for (i = 1; i < 60; i++);
+        return b;
+    }
+
+    int convert() {
+        volatile int i;
+        int j;
+        writeByte(0x44);
+        for (j = 1; j < 1000; j++) {
+            for (i = 1; i < 900; i++) {
+        };
+        if (readBit() == 1)
+            break;
+        };
+        return (j);
+    }
+
+    int readByte() {
+        int byte = 0;
+        int i;
+        for (i = 0; i < 8; i++) {
+            byte = byte | readBit() << i;
+        };
+        return byte;
+    }
+}
---
+++ main.ts
@@ -0,0 +1,100 @@
+//% color=#5042f4 icon="\uf2c9"
+
+namespace DS18B20 {
+    /**
+     * Liest die Temperatur aus dem one-wire Temperature Sensor.
+     * Gibt eine 4-stellige Zahl zurück. Der Wert sollte durch 100 geteilt werden da 
+     * der Wert um 100 überhöht ist. 
+     * Die beiden linken Ziffern sind die Stellen vor dem Komma, die beiden rechten Stellen 
+     * zwei Nachkommastellen.
+     * Originalerweiterung von https://github.com/mengbishu/pxt-ds18b20
+     * Special thanks 2 mengbishu!
+     * Erweitert mit Anpassungen für den Calliope Mini (https://www.calliope.cc) von Michael Klein.
+     * Pins angepasst, Funktionen übersetzt, Writetemperature ergänzt.
+     */
+
+    export enum pin {
+       //% block=P0
+       pin12 = 0,
+       //% block=P1
+       pin0 = 1,
+       //% block=P2
+       pin1 = 2,
+       //% block=P3
+       pin16 = 3,
+        //% block=C16      bekomme die Pins im C++ File einfach nicht angesteuert (Issue1)
+        pin2 = 4,
+        //% block=C17
+        pin8 = 5,
+        //% block=C18
+        pin18 = 6,
+        //% block=C19
+        pin19 = 7
+}
+    
+    //% shim=DS18B20::Temperature
+    export function Temperature(p: number): number {
+        // Fake function for simulator
+        return 0
+    }
+    
+    /**
+    * Schreibt die aktuelle Temperatur auf dem 5x5 Display
+    * und hängt Grad Celsius an */
+    //% weight=10 blockId="WriteTemperature" 
+    //% block="WriteTemperature |%p|"
+    //% p.fieldEditor="gridpicker" p.fieldOptions.columns=4
+    export function WriteTemperature(p: pin): void {
+        basic.showString(TemperatureString(p))
+        images.createImage(`
+        . # . . .
+        # . # . .
+        . # . . .
+        . . . . .
+        . . . . .
+        `).scrollImage(1, 200)
+        basic.showString("C ")
+    }
+    
+   /**
+    * Liest die Temperatur aus dem ds18b20 Sensor als 4-stellige Ganzzahl aus.
+    * Der Wert muss durch 100 geteilt werden um den wirklichen Wert zuberechnen.
+    * Die beiden rechten Ziffern sind die zwei Nachkommastellen!*/
+    //% weight=10 blockId="Temperature_number" 
+    //% block="Temperature_number |%p|"
+    //% p.fieldEditor="gridpicker" p.fieldOptions.columns=4
+    export function TemperatureNumber(p: pin): number {
+        // Fake function for simulator
+        return Temperature(p)
+    }    
+    
+   /**
+    * Liest die Temperatur aus dem ds18b20 aus und erzeugt eine Textzeile (String)
+    mit Komma und 2 Nachkommastellen.*/
+    //% weight=10 blockId="Temperature_string" 
+    //% block="|%p| Temperature_string "
+    //% p.fieldEditor="gridpicker" p.fieldOptions.columns=4
+    export function TemperatureString(p:pin): string {
+     let temp = Temperature(p);
+     let x = (temp / 100)
+     let y = (temp % 100)
+     let z = ''
+      if (temp >= 0) {
+        if (y < 10) {
+            z = x.toString() + ',0' + y.toString()
+        }
+        else {
+            z = x.toString() + ',' + y.toString()
+        }
+    }
+    else if (temp < 0) {
+        if (y > -10) {
+            z = '-' + (-x).toString() + ',0' + (-y).toString()
+        }
+        else {
+            z = '-' + (-x).toString() + ',' + (-y).toString()
+        }
+    }
+    return z
+   }
+}
